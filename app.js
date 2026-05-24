7793c16cca650cedc3fc63b7c6c39738478a02d0
DhanusH200125 <dhanushanand.63@gmail.com>
2026-05-23 19:03:36 -0500

    defined methods

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const userModel = require('./usermodel');
 
 
+// CRUD Operations
 
+// Create 
 app.get('/create', async(req, res)   => {
     let createUser = await userModel.create({
         name: "hasi",
@@ -13,7 +15,14 @@ app.get('/create', async(req, res)   => {
     res.send(createUser);
 }) 
 
+// Read method - find() - to read all the data from the database, findOne() - to read a single document from the database, findById() - to read a document by its unique identifier.
+app.get('/read', async(req,res)=>{
+    let readUser = await userModel.find();
 
+    res.send(readUser);
+})
+
+//Update method - findOneAndUpdate() - to update a single document based on a condition, findByIDAndUpdate() - to update a document by its unique identifier.
 app.get('/update', async(req, res)  => {
    let updateUser = await userModel.findOneAndUpdate(
     {username: "harsh123"},
@@ -24,14 +33,7 @@ app.get('/update', async(req, res)  => {
     res.send(updateUser);
 })
 
-
-app.get('/read', async(req,res)=>{
-    let readUser = await userModel.find();
-
-    res.send(readUser);
-})
-    
-
+// Delete method - findOneAndDelete() - to delete a single document based on a condition, findByIdAndDelete() - to delete a document by its unique identifier.
 app.get('/delete', async(req, res)=>{
     let deleteUser = await userModel.findOneAndDelete({name: "harsh sharma"});
 
