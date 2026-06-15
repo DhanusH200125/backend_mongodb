0b8979167ef24892cb83efec596c67ac36c9d0e7
DhanusH200125 <dhanushanand.63@gmail.com>
2026-06-15 05:31:20 -0500

    Add Crud_ejs_SSrendering project with Express, mongoose and EJS

--- CRUD_EJS_and_SSRendering/app.js
+++ CRUD_EJS_and_SSRendering/app.js
@@ -1,12 +1,70 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const userModel = require('./models/users');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.render("index");
 
 })
 
+app.get('/read', async(req, res) => {
+    let allusers = await userModel.find();
+    res.render("read", {users: allusers});
+})
+
+app.post('/create', async(req, res) => {
+    let {username, email, imageUrl} = req.body;
+    let createdUser = await userModel.create({
+        username: username,
+        email: email,
+        imageUrl: imageUrl,
+    })
+    res.redirect('/read');
+    // res.send(createdUser);
+})
+
+app.get('/delete/:_id', async(req, res) =>{
+    let {_id} = req.params;
+    let allusers = await userModel.findOneAndDelete({_id: _id});
+    res.redirect('/read');
+
+})
+
+app.get('/edit/:userId', async(req, res) =>{
+    // let {_id: userId} = req.params; 
+    let user = await userModel.findOne({_id: req.params.userId});
+    res.render('edit', {user: user});
+
+
+})
+
+
+app.post('/update/:userId', async(req, res) =>{ 
+    let user = await userModel.findOneAndUpdate({_id: req.params.userId}, {
+        username: req.body.username,
+        email: req.body.email,
+        imageUrl: req.body.imageUrl,
+    }, {new: true}
+    );
+    res.redirect("/read");
+
+})
+
+app.get('/check-data', async(req, res) => {
+    let allusers = await userModel.find();
+    console.log(req.params);
+    res.json(allusers);  // This will show raw data in json format
+})
+
+
+
+
 app.listen(8000);
+
---
+++ CRUD_EJS_and_SSRendering/models/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/testusers');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    imageUrl: String,
+})
+
+module.exports= mongoose.model('User', userSchema);
